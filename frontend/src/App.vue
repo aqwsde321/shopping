621912c18ed4162459bd27598558a37561430cd6
qrqrq23r <qrqrq23r@gmail.com>
2024-12-22 22:23:50 +0900

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import Footer from './components/Footer.vue';
+import Header from './components/Header.vue';
+import Home from './pages/Home.vue';
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Home,
+    Footer,
+  }
+}
+
+
+
+</script>
+
+<template>
+
+  <Header />
+
+  <Home />
+
+  <Footer />
+
+</template>
