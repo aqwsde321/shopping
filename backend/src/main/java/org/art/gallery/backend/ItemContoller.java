77e3ae953dee39bf4ba91796ff1bf97c9671270b
qrqrq23r <qrqrq23r@gmail.com>
2024-12-22 23:51:14 +0900

    vue proxy 설정 및 spring boot api 연결 테스트

--- backend/src/main/java/org/art/gallery/backend/ItemContoller.java
+++ backend/src/main/java/org/art/gallery/backend/ItemContoller.java
@@ -1,5 +1,22 @@
 package org.art.gallery.backend;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class ItemContoller {
-    
+
+    @GetMapping("/api/items")
+    public List<String> getItems(){
+        System.out.println("get api");
+        List<String> items = new ArrayList<>();
+        items.add("a");
+        items.add("b");
+        items.add("c");
+
+        return items;
+    }
 }
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -21,8 +21,9 @@
             <div class="container">
 
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div class="col" v-for="i in 12" :key="i">
-                        <Card />
+                    <div class="col" v-for="(item, idx) in state.items" :key="idx">
+
+                        <Card :item="item" />
                     </div>
                 </div>
             </div>
@@ -33,12 +34,24 @@
 </template>
 
 <script>
+import axios from 'axios';
 import Card from '../components/Card.vue'
+import { reactive } from 'vue';
 
 export default {
     name: 'Home',
     components: {
         Card,
+    },
+    setup() {
+        const state = reactive({
+            items: []
+        })
+        axios.get('/api/items').then(({ data }) => {
+
+            state.items = data;
+        })
+        return { state }
     }
 }
 </script>
