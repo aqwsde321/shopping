0a1b0331a1a846bdcdb9693a4fe4f95804ebffce
aqwsde321 <qrqrq23r@gmail.com>
2024-12-23 23:28:19 +0900

    be: order 로직 추가

---
+++ backend/src/main/java/org/art/gallery/backend/controller/OrderContoller.java
@@ -0,0 +1,68 @@
+package org.art.gallery.backend.controller;
+
+import jakarta.transaction.Transactional;
+import org.art.gallery.backend.dto.OrderDto;
+import org.art.gallery.backend.entity.Order;
+import org.art.gallery.backend.repository.CartRepository;
+import org.art.gallery.backend.repository.OrderRepository;
+import org.art.gallery.backend.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+public class OrderContoller {
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    JwtService jwtService;
+
+    @GetMapping("/api/orders")
+    public ResponseEntity getOrder(
+            @CookieValue(value = "token", required = false) String token
+    ) {
+
+        if (!jwtService.isValid(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        int memberId = jwtService.getId(token);
+        List<Order> orders = orderRepository.findByMemberIdOrderByIdDesc(memberId);
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+    @Transactional
+    @PostMapping("/api/orders")
+    public ResponseEntity pushOrder(
+            @RequestBody OrderDto dto,
+            @CookieValue(value = "token", required = false) String token
+    ) {
+
+        if (!jwtService.isValid(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        int memberId = jwtService.getId(token);
+        Order newOrder = new Order();
+
+        newOrder.setMemberId(memberId);
+        newOrder.setName(dto.getName());
+        newOrder.setAddress(dto.getAddress());
+        newOrder.setPayment(dto.getPayment());
+        newOrder.setCardNumber(dto.getCardNumber());
+        newOrder.setItems(dto.getItems());
+
+        orderRepository.save(newOrder);
+       // cartRepository.deleteByMemberId(memberId);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ backend/src/main/java/org/art/gallery/backend/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package org.art.gallery.backend.repository;
+
+import org.art.gallery.backend.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+    List<Order> findByMemberIdOrderByIdDesc(int memberId);
+}
