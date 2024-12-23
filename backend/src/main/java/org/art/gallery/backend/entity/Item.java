1a5e342e0b65ce1ca1b53e8f67d9387a2ddc43cb
qrqrq23r <qrqrq23r@gmail.com>
2024-12-23 14:13:33 +0900

    item 컬럼 추가 및 출력

--- backend/src/main/java/org/art/gallery/backend/entity/Item.java
+++ backend/src/main/java/org/art/gallery/backend/entity/Item.java
@@ -10,7 +10,17 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(length=50, nullable= false)
     private String name;
+
+    @Column(length=100)
+    private String imgPath;
+
+    @Column
+    private int price;
+
+    @Column
+    private int discountPer;
 
 }
---
+++ frontend/src/scripts/lib.js
@@ -0,0 +1,5 @@
+export default {
+    getNumberFormatted(val) {
+        return val.toLocaleString();
+    }
+}
